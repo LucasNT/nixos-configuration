6b8814115aeb2544e5b1394d33e23254cc013cf4
LucasNT <lucasntakahashi@gmail.com>
2025-08-24 10:56:02 -0300

    feat(momo): add transmission as container instead ...
    of a nixpkg

--- hosts/momo/containers.nix
+++ hosts/momo/containers.nix
@@ -6,4 +6,21 @@
     ports = [ "5230:5230" ];
     image = "neosmemo/memos:stable";
   };
+
+  virtualisation.oci-containers.containers.transmission = {
+    volumes = [
+      "/files/media/transmission/data:/config"
+      "/files/media/transmission/Dowloads:/downloads"
+    ];
+    image = "lscr.io/linuxserver/transmission:latest";
+    environment = {
+      PUID = 2005;
+      GUID = 2005;
+      TZ = "America/Sao_Paulo";
+    };
+    ports = [ "9091:9091" "51413:51413" "51413:51413/udp" ];
+  };
+
+  networking.firewall.allowedTCPPorts = [ 9091 51413 5230 ];
+  networking.firewall.allowedUDPPorts = [ 51413 ];
 }
