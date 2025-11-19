7f644a9edeea95d56210efdc5b320f45574673db
LucasNT <lucasntakahashi@gmail.com>
2025-11-18 22:36:08 -0300

    fix(modules): correct updateSystem module
    Fix incorrect OnCalendar field
    Fix incorrect update script

--- modules/updateSystem.nix
+++ modules/updateSystem.nix
@@ -8,9 +8,8 @@ in {
   config = lib.mkIf cfg.enable {
     systemd.timers."update-system" = {
       timerConfig = {
-        OnCalendar = "00 10 * * 0,3,6";
+        OnCalendar = "Mon,Wed,Fri,Sun *-*-* 10:00:00";
         Persistent = true;
-        Unit = "update-system";
       };
     };
 
@@ -18,11 +17,11 @@ in {
       script = ''
         set -eu
         ${pkgs.coreutils}/bin/sleep 60
-        ${pkgs.libnotify}/bin/notify-send -u normal "Nix Rebuild Started"
-        if ${pkgs.nixos-rebuild}/bin/nixos-rebuild --flake git://github.com/LucasNT/nixos-configuration:main --verbose boot; then
-            ${pkgs.libnotify}/bin/notify-send -u normal "Nix Rebuild Succed"
+        ${pkgs.coreutils}/bin/echo "Nix Rebuild Started"
+        if ${pkgs.nixos-rebuild}/bin/nixos-rebuild --flake github:LucasNT/nixos-configuration --verbose boot; then
+            ${pkgs.coreutils}/bin/echo "Nix Rebuild Succed"
         else
-            ${pkgs.libnotify}/bin/notify-send -u critical "Nix Rebuild Failed"
+            ${pkgs.coreutils}/bin/echo "Nix Rebuild Failed"
         fi
       '';
       serviceConfig = {
