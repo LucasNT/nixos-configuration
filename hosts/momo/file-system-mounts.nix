10e395aba701c08f35858fcd52b7f840302f861d
LucasNT <lucasntakahashi@gmail.com>
2025-12-24 21:13:30 -0300

    feat(momo): add nfs export for proxmox

--- hosts/momo/file-system-mounts.nix
+++ hosts/momo/file-system-mounts.nix
@@ -31,11 +31,18 @@
     options = [ "subvol=@media-server" ];
   };
 
+  fileSystem."/files/vmBackups" = {
+    device = "/dev/disk/by-uuid/b0f49523-ab2d-4c9a-9364-831463616ebe";
+    fsType = "btrfs";
+    options = [ "subvol=@vmBackups" ];
+  };
+
   services.nfs.server = {
     enable = true;
     exports = ''
       /files/Lucas 192.168.189.9(rw,nohide,subtree_check) 192.168.189.8(rw,nohide,subtree_check) 192.168.189.4(rw,nohide,subtree_check) 192.168.135.3(rw,nohide,subtree_check)
       /files/media 192.168.189.9(rw,nohide,subtree_check) 192.168.189.8(rw,nohide,subtree_check) 192.168.189.4(rw,nohide,subtree_check) 192.168.135.3(rw,nohide,subtree_check) 192.168.189.82(rw,nohide,subtree_check)
+      /files/vmBackups 192.168.189.5(rw,nohide,subtree_check)
     '';
   };
 }
