f54e2c5e9b51f81443db61f410c48ce2b4301b31
LucasNT <lucasntakahashi@gmail.com>
2026-03-13 08:41:18 -0300

    Revert "feat(visio-note): add devenv"
    This reverts commit 7d52619cf167ae33745905e2618c25e9fb77a7e6.

--- hosts/visio-note/configuration.nix
+++ hosts/visio-note/configuration.nix
@@ -23,7 +23,6 @@ in {
       bitwarden-cli
       borgbackup
       chromium
-      devenv
       discord
       gh
       gdk
@@ -68,11 +67,6 @@ in {
       }];
     };
   };
-
-  nix.extraOptions = ''
-    extra-substituters = https://devenv.cachix.org
-    extra-trusted-public-keys = devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=
-  '';
 
   nixpkgs = {
     config = {
