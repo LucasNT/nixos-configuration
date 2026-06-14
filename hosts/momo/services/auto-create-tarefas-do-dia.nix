3aa848578d0761f99c49e1e41f79befae961fbfe
LucasNT <lucasntakahashi@gmail.com>
2026-06-14 20:17:42 -0300

    fix(momo): correct  auto nota creator

--- hosts/momo/services/auto-create-tarefas-do-dia.nix
+++ hosts/momo/services/auto-create-tarefas-do-dia.nix
@@ -12,7 +12,7 @@
   systemd.services."auto-create-tarefas-do-dia" = {
     script = ''
       export PATH="${pkgs.coreutils}/bin:${pkgs.curl}/bin"
-      curl -X POST -H "Authorization: Bearer $(cat /home/memo_token)" -H "Content-Type: application/json" -d '{"name":"atividade diárias", "state":"NORMAL", "content":"#Objetivos/Dia\n- [ ] Aguar as plantas\n- [ ] Termo\n-[ ] Dueto\n-[ ] Quarteto\n- [ ] Xadrez\n- [ ] Opacha MD\n", visibility: "PRIVATE"}'  https://notas.geladeira.moe/api/v1/memos
+      curl -X POST -H "Authorization: Bearer $(cat /home/memo_token)" -H "Content-Type: application/json" -d '{"name":"atividade diárias", "state":"NORMAL", "content":"#Objetivos/Dia\n- [ ] Aguar as plantas\n- [ ] Termo\n- [ ] Dueto\n- [ ] Quarteto\n- [ ] Xadrez\n- [ ] Opacha MD\n", "visibility": "PRIVATE"}'  https://notas.geladeira.moe/api/v1/memos
     '';
     serviceConfig = {
       Type = "oneshot";
