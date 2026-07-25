d4a0cbedd9e9904b7cb28c72d11278cffe5be695
LucasNT <lucasntakahashi@gmail.com>
2026-07-24 22:55:14 -0300

    fix(momo): trying to fix redis

--- hosts/momo/file-system-mounts.nix
+++ hosts/momo/file-system-mounts.nix
@@ -43,7 +43,7 @@
     options = [ "subvol=@applications/@Garage"];
   };
 
-  fileSystems."/files/application/redis/anytype" = {
+  fileSystems."/var/lib/redis-anytype" = {
     device = "/dev/disk/by-uuid/b0f49523-ab2d-4c9a-9364-831463616ebe";
     fsType = "btrfs";
     options = [ "subvol=@applications/@AnytypeRedis"];
--- hosts/momo/services/redis.nix
+++ hosts/momo/services/redis.nix
@@ -1,12 +1,11 @@
 { config, lib, pkgs, ... }:
 
 {
-  networking.firewall.allowedTCPPorts = [ 64265 ];
   services.redis.servers.anytype = {
     enable = true;
     appendOnly = true;
     bind = "0.0.0.0";
     port = 44265;
-    settings. dir = lib.mkForce "/files/application/redis/anytype";
+    openFirewal = true;
   };
 }
